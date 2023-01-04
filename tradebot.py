a48565fdc626d5402fef913ab3695fbb0e0306d5
Thabo Sodi <thabosodi11@gmail.com>
2023-01-04 12:46:58 +0200

    download the data

--- tradebot.py
+++ tradebot.py
@@ -1,4 +1,6 @@
 import yfinance as yf
 import pandas as pd
 
-dataF = yf.download("EURUSD=X", start="2022-10-4", end)
+dataF = yf.download("EURUSD=X", start="2022-10-4", end="2022-12-2", interval='15m')
+dataF.iloc[-1:,:]
+dataF.Open.iloc
